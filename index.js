9b9c5e8bc885c5920a2a5a5ed4d6f2b2d8f420a4
Musyoka552 <kilonzopatoh6@gmail.com>
2024-06-29 14:32:58 +0300

    arrow functions

---
+++ index.js
@@ -0,0 +1,14 @@
+function divide(){
+let a = 2000;
+let b = 100;
+return a/b
+}
+let x = 2;
+let square =(square) =>{
+    return x*x 
+}
+let a = 3;
+let b = 4;
+let add = (add) =>{
+    return a+b
+}
